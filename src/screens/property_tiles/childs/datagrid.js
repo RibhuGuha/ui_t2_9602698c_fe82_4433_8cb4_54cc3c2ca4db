d9fae8a3d46d0d200e40bb2117db4e2d82a953a9
RibhuGuha <ribhu@replicacia.com>
2024-05-10 11:05:17 +0530

    initial commit

---
+++ src/screens/property_tiles/childs/datagrid.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { TablePagination, Grid, Typography } from '@mui/material';
+import { ROWSPERPAGE } from "config";
+
+import { GridContainer, CardItem } from "components";
+
+const Component = (props) => {
+
+    const { rowsCount, rows, pageInfo, onActionClicked, onPageClicked } = props;
+
+    const handleChangePage = (event, newPage) => {
+        const _page = { page: newPage, pageSize: pageInfo.pageSize };
+        if (onPageClicked) onPageClicked(_page);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        /* setPageInfo({ page: 0, pageSize: parseInt(event.target.value, 5) }); */
+    };
+
+    const OnActionClicked = (id, type) => {
+        if (onActionClicked) onActionClicked(id, type);
+    };
+
+        MainImage
+        Property
+        Title
+        Property
+        Description2
+        Property
+        Address
+        Property
+
+    return (
+        <>
+            <GridContainer>
+                {rows && rows.map((x, index) => (
+                    <CardItem key={index} keyid={x.PropId} title={x.$titleProp} imgsrc={x.ProductMainImageData} width={300}
+                        description={x.$descProp} onActionClicked={OnActionClicked}>
+                    <Grid container direction="column">
+                    </Grid>
+                                                                                                                                                                                                                    </CardItem>
+                ))}
+            </GridContainer>
+            {rows && rows.length > 0 && <TablePagination
+                component="div"
+                count={rowsCount}
+                page={pageInfo.page}
+                rowsPerPageOptions={ROWSPERPAGE}
+                onPageChange={handleChangePage}
+                rowsPerPage={pageInfo.pageSize}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />}
+        </>
+    );
+
+};
+
+export default Component;
